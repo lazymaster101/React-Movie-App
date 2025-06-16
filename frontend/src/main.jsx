95dfc2fbf03d599a28f564a85adeda7d7e4f2ed9
lazymaster101 <seshadithya01@gmail.com>
2025-06-16 13:04:52 -0500

    Changing website name

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter basename='/React-Movie-App'>
+        <BrowserRouter basename='/React-Movie-App/'>
             <App />
         </BrowserRouter>
         
